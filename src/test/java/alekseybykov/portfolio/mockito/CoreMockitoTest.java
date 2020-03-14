bff3b7ce7596ed5861c80c8b3b6fcee4eda41535
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-03-14 20:26:46 +0400

    Added additional tests for mocked get method.

--- src/test/java/alekseybykov/portfolio/mockito/CoreMockitoTest.java
+++ src/test/java/alekseybykov/portfolio/mockito/CoreMockitoTest.java
@@ -2,6 +2,7 @@ package alekseybykov.portfolio.mockito;
 
 import org.apache.commons.lang3.math.NumberUtils;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.*;
 
@@ -25,6 +26,26 @@ public class CoreMockitoTest {
 		when(list.get(0)).thenReturn(NumberUtils.INTEGER_ONE);
 
 		assertEquals(NumberUtils.INTEGER_ONE, list.get(0));
+	}
+
+	@Test
+	public void testMockGetByAnyIdxCall() {
+		List<Integer> list = mockList();
+		Mockito.when(list.get(Mockito.anyInt())).thenReturn(NumberUtils.INTEGER_ZERO);
+
+		assertEquals(NumberUtils.INTEGER_ZERO, list.get(0));
+		assertEquals(NumberUtils.INTEGER_ZERO, list.get(1));
+		assertEquals(NumberUtils.INTEGER_ZERO, list.get(100));
+	}
+
+	@Test
+	public void testMockThreeGetCalls() {
+		List<Integer> list = mockList();
+		when(list.size()).thenReturn(-1).thenReturn(0).thenReturn(1);
+
+		assertEquals(-1, list.size());
+		assertEquals(0, list.size());
+		assertEquals(1, list.size());
 	}
 
 	@SuppressWarnings("unchecked")

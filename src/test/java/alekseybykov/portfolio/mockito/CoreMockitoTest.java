946ba49b6e37b39352aafe35409085fd2e72cffc
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-03-14 16:42:17 +0400

    Added additional test for mocked get method.

--- src/test/java/alekseybykov/portfolio/mockito/CoreMockitoTest.java
+++ src/test/java/alekseybykov/portfolio/mockito/CoreMockitoTest.java
@@ -1,7 +1,9 @@
 package alekseybykov.portfolio.mockito;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.junit.Test;
-import java.util.Map;
+
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -10,10 +12,23 @@ import static org.mockito.Mockito.when;
 public class CoreMockitoTest {
 
 	@Test
-	public void testSetAndCheckSizeOfMock() {
-		Map mapMock = mock(Map.class);
-		when(mapMock.size()).thenReturn(1);
+	public void testMockSizeCall() {
+		List<Integer> list = mockList();
+		when(list.size()).thenReturn(1);
 
-		assertEquals(1, mapMock.size());
+		assertEquals(1, list.size());
+	}
+
+	@Test
+	public void testMockGetCall() {
+		List<Integer> list = mockList();
+		when(list.get(0)).thenReturn(NumberUtils.INTEGER_ONE);
+
+		assertEquals(NumberUtils.INTEGER_ONE, list.get(0));
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T> List<T> mockList() {
+		return mock(List.class);
 	}
 }

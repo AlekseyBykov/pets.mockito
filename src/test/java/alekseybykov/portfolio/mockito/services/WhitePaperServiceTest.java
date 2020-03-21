ae59fad98a9d9177254a3ee837981329c3a1b734
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-03-21 13:49:58 +0400

    Refactoring using annotations and DI.

--- src/test/java/alekseybykov/portfolio/mockito/services/WhitePaperServiceTest.java
+++ src/test/java/alekseybykov/portfolio/mockito/services/WhitePaperServiceTest.java
@@ -1,44 +1,61 @@
 package alekseybykov.portfolio.mockito.services;
 
 import alekseybykov.portfolio.mockito.dependencies.external.DocumentService;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class WhitePaperServiceTest {
 
+	@Mock
+	private DocumentService documentService;
+	@Captor
+	private ArgumentCaptor<String> argumentCaptor;
+	@InjectMocks
+	private WhitePaperService whitePaperService;
+
 	private static List<String> whitePapers;
-	private static ArgumentCaptor<String> argumentCaptor;
+	/*private static ArgumentCaptor<String> argumentCaptor;*/
 
 	@BeforeClass
 	public static void setup() {
 		whitePapers = Arrays.asList("Document1 rev.01", "Document2 rev.02", "Document1 rev.02", "Document4 rev.03");
-		argumentCaptor = ArgumentCaptor.forClass(String.class);
+		/*argumentCaptor = ArgumentCaptor.forClass(String.class);*/
 	}
 
 	@Test
 	public void testFilterWhitePapers() {
-		DocumentService documentService = mock(DocumentService.class);
+
+		/*DocumentService documentService = mock(DocumentService.class);*/
 		String whitePaperName = "Document1";
 
 		when(documentService.searchDocumentsByName(whitePaperName)).thenReturn(whitePapers);
 
-		WhitePaperService whitePaperService = new WhitePaperService(documentService);
+		/*WhitePaperService whitePaperService = new WhitePaperService(documentService);*/
 		List<String> whitePapers = whitePaperService.filterWhitePapers(whitePaperName);
 
 		assertEquals(2, whitePapers.size());
@@ -46,13 +63,13 @@ public class WhitePaperServiceTest {
 
 	@Test
 	public void testDeleteWhitePapers() {
-		DocumentService documentService = mock(DocumentService.class);
+		/*DocumentService documentService = mock(DocumentService.class);*/
 
 		String whitePaperName = "Document1";
 
 		when(documentService.searchDocumentsByName(whitePaperName)).thenReturn(whitePapers);
 
-		WhitePaperService whitePaperService = new WhitePaperService(documentService);
+		/*WhitePaperService whitePaperService = new WhitePaperService(documentService);*/
 
 		whitePaperService.deleteWhitePapers(whitePaperName);
 
@@ -75,13 +92,18 @@ public class WhitePaperServiceTest {
 		then(documentService).should(never()).deleteDocumentByName("Document4 rev.03");
 
 		verify(documentService, times(2)).deleteDocumentByName(argumentCaptor.capture());
-		assertThat(argumentCaptor.getAllValues().size(), is(2));
+
+		List<String> arguments = argumentCaptor.getAllValues();
+
+		assertThat(arguments.size(), is(2));
+		assertThat(arguments, hasSize(2));
+		assertThat(arguments, everyItem(not(StringUtils.EMPTY)));
 	}
 
 	@Test
 	public void testFilterWhitePapersUsingBddStyle() {
-		DocumentService documentService = mock(DocumentService.class);
-		WhitePaperService whitePaperService = new WhitePaperService(documentService);
+		/*DocumentService documentService = mock(DocumentService.class);*/
+		/*WhitePaperService whitePaperService = new WhitePaperService(documentService);*/
 		
 		String whitePaperName = "Document2";
 
